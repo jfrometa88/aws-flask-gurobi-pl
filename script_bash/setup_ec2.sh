2b560f7e38a9bd980a34224391061fd2f1e7eece
Jorge Israel Frómeta Moya <jfrometa99@gmail.com>
2025-02-11 20:28:59 +0100

    Add files via upload

---
+++ script_bash/setup_ec2.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Actualizar el sistema
+sudo yum update -y
+
+# Instalar AWS CLI
+sudo yum install aws-cli -y
+
+# Instalar pip, el gestor de paquetes de Python
+sudo yum install -y python3-pip
+
+# Instalar Flask
+pip3 install flask
+
+# Instalar Boto3
+pip3 install boto3
+
+# Instalar GurobiPy
+pip3 install gurobipy
+
+# Instalar Matplotlib
+pip3 install matplotlib
+
+# Descargar y configurar Gurobi (Solo si se tiene una licencia)
+cd /home/ec2-user
+wget https://packages.gurobi.com/10.0/gurobi10.0.1_linux64.tar.gz
+tar -xvzf gurobi10.0.1_linux64.tar.gz
+sudo mv gurobi10.0.1_linux64 /opt/gurobi
+echo "export GUROBI_HOME=\"/opt/gurobi\"" >> ~/.bashrc
+echo "export PATH=\"\$GUROBI_HOME/bin:\$PATH\"" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=\"\$GUROBI_HOME/lib:\$LD_LIBRARY_PATH\"" >> ~/.bashrc
+source ~/.bashrc
+
+# Configurar la licencia de Gurobi (Sustituye XXXXX por la clave)
+grbgetkey XXXXXXXX-XXXXXXXX-XXXXXXXX
+
+
+
+
